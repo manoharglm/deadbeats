0812c5ed5a74a444a1d62e3d28dac971c3a15b64
thecodeabides <manohargunduboina@gmail.com>
2020-10-24 19:07:09 +0530

    Add assets

--- deadbeats/ContentView.swift
+++ deadbeats/ContentView.swift
@@ -6,12 +6,37 @@
 //  Copyright © 2020 thecodeabides. All rights reserved.
 //
 
+import UIKit
 import SwiftUI
+//import PlaygroundSupport
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+    @State var isShowingText: Bool = true
+      
+      var body: some View {
+          Group {
+              Spacer()
+              Toggle(isOn: $isShowingText) {
+                  Text("Show Text")
+              }
+              Spacer()
+              
+              (self.isShowingText)
+                ? Text("I love to toggle").bold().foregroundColor(Color.red)
+                  : nil
+              (self.isShowingText)
+                  ? Text("Yes blinking is so great")
+                  : nil
+              (self.isShowingText)
+                  ? Text("Why did they ever take this out of Swift")
+                  : nil
+              (self.isShowingText)
+                  ? Text("Look at me look at me look at me")
+                  : nil
+              Spacer()
+          }
+          
+      }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +44,8 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+//PlaygroundPage.current.liveView = UIHostingController(rootView: Blink())
--- deadbeats/SceneDelegate.swift
+++ deadbeats/SceneDelegate.swift
@@ -62,3 +62,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+
